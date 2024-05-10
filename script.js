ad18fd0590f51192177b1851ddc8d8a87afdf4b8
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-05-10 14:02:15 -0500

    remove second hand

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const body = document.querySelector("body"),
-  hourHand = document.querySelector(".hour"),
+const hourHand = document.querySelector(".hour"),
   minuteHand = document.querySelector(".minute"),
   secondHand = document.querySelector(".second");
 
 const updateTime = () => {
   let date = randomDate(),
-    secToDeg = (date.getSeconds() / 60) * 360,
     minToDeg = (date.getMinutes() / 60) * 360,
     hrToDeg = (date.getHours() / 12) * 360;
 
-  secondHand.style.transform = `rotate(${secToDeg}deg)`;
   minuteHand.style.transform = `rotate(${minToDeg}deg)`;
   hourHand.style.transform = `rotate(${hrToDeg}deg)`;
 };
