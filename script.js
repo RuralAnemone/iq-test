64a615597cd795eb58ea16c8f89b28f1c639478d
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-05-10 15:11:15 -0500

    fix rotations, add fine ticks

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const hourHand = document.querySelector(".hour"),
-  minuteHand = document.querySelector(".minute"),
-  secondHand = document.querySelector(".second");
+  minuteHand = document.querySelector(".minute");
 
 const updateTime = () => {
   let date = randomDate(),
@@ -16,3 +15,15 @@ const randomDate = () => {
 }
 
 updateTime();
+
+const clockFace = document.querySelector(".clock");
+
+for (let i = 0; i < 60; i++) {
+  if (i % 5 == 0) continue;
+  const tick = document.createElement("label");
+  const tickText = tick.appendChild(document.createElement("span"));
+  tickText.innerText = "-";
+  tick.classList.add("fine-tick");
+  tick.style.transform = `rotate(${(i * 6)}deg)`;
+  clockFace.appendChild(tick);
+}
