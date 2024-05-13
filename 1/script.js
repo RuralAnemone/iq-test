8dbacc187b0f306ccb6215abc2198a7f018bf4cf
Isaiah W <64103005+RuralAnemone@users.noreply.github.com>
2024-05-13 00:02:48 -0500

    quiz structure

---
+++ 1/script.js
@@ -0,0 +1,33 @@
+const hourHand = document.querySelector(".hour"),
+	minuteHand = document.querySelector(".minute");
+
+const randomTime = Math.floor(Math.random() * 12 * 60); // 0 - 719
+
+const updateTime = () => {
+	let minToDeg = (randomTime % 60) * (360 / 60),
+		hrToDeg = (randomTime / 60) * (360 / 12);
+
+	minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+	hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+updateTime();
+
+const clockFace = document.querySelector(".clock");
+
+for (let i = 0; i < 60; i++) {
+	if (i % 5 == 0) continue;
+	const tick = document.createElement("label");
+	const tickText = tick.appendChild(document.createElement("span"));
+	tickText.innerText = "-";
+	tick.classList.add("fine-tick");
+	tick.style.transform = `rotate(${i * 6}deg)`;
+	clockFace.appendChild(tick);
+}
+
+const timeDisplay = document.querySelector(".time-display");
+const hours = Math.floor(randomTime / 60);
+const minutes = randomTime % 60;
+const hoursString = hours == 0 ? "12" : [] + hours;
+const minutesString = `${minutes < 10 ? "0" : ""}${minutes}`;
+timeDisplay.innerText = `${hoursString}:${minutesString}`;
--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-const hourHand = document.querySelector(".hour"),
-	minuteHand = document.querySelector(".minute");
+let startButton = document.querySelector(".start")
 
-const randomTime = Math.floor(Math.random() * 12 * 60); // 0 - 719
+startButton.addEventListener("mouseenter", () => {
+	startButton.innerText = "start quiz!";
+});
 
-const updateTime = () => {
-	let minToDeg = (randomTime % 60) * (360 / 60),
-		hrToDeg = (randomTime / 60) * (360 / 12);
-
-	minuteHand.style.transform = `rotate(${minToDeg}deg)`;
-	hourHand.style.transform = `rotate(${hrToDeg}deg)`;
-};
-
-updateTime();
-
-const clockFace = document.querySelector(".clock");
-
-for (let i = 0; i < 60; i++) {
-	if (i % 5 == 0) continue;
-	const tick = document.createElement("label");
-	const tickText = tick.appendChild(document.createElement("span"));
-	tickText.innerText = "-";
-	tick.classList.add("fine-tick");
-	tick.style.transform = `rotate(${i * 6}deg)`;
-	clockFace.appendChild(tick);
-}
-
-const timeDisplay = document.querySelector(".time-display");
-const hours = Math.floor(randomTime / 60);
-const minutes = randomTime % 60;
-const hoursString = hours == 0 ? "12" : [] + hours;
-const minutesString = `${minutes < 10 ? "0" : ""}${minutes}`;
-timeDisplay.innerText = `${hoursString}:${minutesString}`;
+startButton.addEventListener("mouseout", () => {
+	startButton.innerText = "start quiz";
+});
