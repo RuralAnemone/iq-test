be6d80f28ece116b1a0b3b2853038992a28d28de
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-05-14 13:59:19 -0500

    express app

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+app.use(express.json()); // Add this line to parse JSON bodies
+
+app.post('/upload', (req, res) => {
+  // Handle the POST request to /upload here
+  res.send('File uploaded successfully!');
+  console.log(req)
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
