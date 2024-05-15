43b106b301ced828a51ca71f4f9c23d1fcc8b995
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-05-15 16:23:22 -0500

    upload data to test sheet

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const process = require('process');
 const sheetdb = require('sheetdb-node');
 
-const client = sheetdb("https://sheetdb.io/api/v1/58f61be4dda40");
+const client = sheetdb({address:"https://sheetdb.io/api/v1/58f61be4dda40"});
 
 const expressApp = express();
 const publicPath = path.join(__dirname, 'public');
@@ -17,13 +17,25 @@ expressApp.use(express.static(publicPath));
 expressApp.use(express.json()); // Add this line to parse JSON bodies
 
 expressApp.post('/upload', async (req, res) => {
-	res.status(202).send('accepted');
-	const message = req.body;
+	// res.status(202).send('accepted');
+	// const message = req.body;
 	// await webhook(JSON.stringify(message));
 	// try and catch errors
 	try {
 		// await webhook(JSON.stringify(message));
-		await appendDataToSheet([JSON.stringify(message)]);
+		await appendDataToSheet({
+
+			"id": "INCREMENT",
+			"name": "Mike Rotchburns",
+			"age": Math.floor(Math.random() * 10000 + 1),
+			"score": Math.floor(Math.random() * 100 + 1),
+			"address": JSON.stringify(req.body),
+			"email": "mikey@rotchburns.gov",
+			"lat": Math.random() * 180 - 90,
+			"lon": Math.random() * 360 - 180,
+			"img": "https://placebear.com/200/200"
+		}, "large");
+		res.status(200).send('Data appended to sheet');
 	} catch(error) {
 		console.error(error);
 		res.status(500).send(`Error: ${error.message}; ${JSON.stringify(error)}`);
@@ -35,6 +47,6 @@ expressApp.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-function appendDataToSheet(data) {
-
+async function appendDataToSheet(data, sheet) {
+	await client.create(data, sheet);
 }
